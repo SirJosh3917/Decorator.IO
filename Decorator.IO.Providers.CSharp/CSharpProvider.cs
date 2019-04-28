a0c1ed4832b8342936a510a577c53f1f8a2bb297
SirJosh3917 <joshuaneedham@hotmail.com>
2019-04-28 00:08:20 -0400

    cleanup casing and calls to compilation unit

--- Decorator.IO.Providers.CSharp/CSharpProvider.cs
+++ Decorator.IO.Providers.CSharp/CSharpProvider.cs
@@ -1,7 +1,5 @@
 ﻿using Decorator.IO.Core;
 
-using Humanizer;
-
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -13,35 +11,11 @@ using System.Threading.Tasks;
 
 namespace Decorator.IO.Providers.CSharp
 {
-	public class CSharpCasing
-	{
-		// i don't like changing stuff by reference
-		// but i'm lazy lol
-
-		public void Apply(DecoratorFile @in)
-		{
-			@in.Namespace = @in.Namespace.Split('.')
-				.Select(x => x.Pascalize())
-				.Aggregate((a, b) => $"{a}.{b}");
-
-			foreach (var x in @in.Classes)
-			{
-				x.Name = x.Name.Pascalize();
-			}
-
-			foreach (var i in @in.Classes.SelectMany(x => x.Fields))
-			{
-				i.Name = i.Name.Pascalize();
-			}
-		}
-	}
-
 	public class CSharpProvider : IProvider
 	{
 		public async Task Provide(Stream outputStream, DecoratorFile file)
 		{
-			new CSharpCasing()
-				.Apply(file);
+			file.ApplyCSharpCasing();
 
 			var result = SyntaxFactory.CompilationUnit()
 				.WithMembers
@@ -56,14 +30,12 @@ namespace Decorator.IO.Providers.CSharp
 								file
 									.Classes
 									.Select(decoratorClass => new InterfaceBuilder().BuildInterface(decoratorClass))
-									.Select(compilationUnit => compilationUnit.ChildNodes().First())
-									.Cast<MemberDeclarationSyntax>()
+									.SelectMany(compilationUnit => compilationUnit.AsMemberDeclarationSyntaxes())
 								.Concat
 								(
 									file.Classes
 										.Select(decoratorClass => new ClassBuilder().BuildClass(decoratorClass))
-										.Select(compilationUnit => compilationUnit.ChildNodes().First())
-										.Cast<MemberDeclarationSyntax>()
+										.SelectMany(compilationUnit => compilationUnit.AsMemberDeclarationSyntaxes())
 								)
 								.Append
 								(
--- Decorator.IO.Providers.CSharp/Helpers.cs
+++ Decorator.IO.Providers.CSharp/Helpers.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.CodeAnalysis.CSharp;
+﻿using Decorator.IO.Core;
+
+using Humanizer;
+
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Decorator.IO.Providers.CSharp
 {
@@ -7,5 +13,29 @@ namespace Decorator.IO.Providers.CSharp
 	{
 		public static CompilationUnitSyntax AsCompilationUnitSyntax(this string str)
 			=> CSharpSyntaxTree.ParseText(str, CSharpParseOptions.Default).GetCompilationUnitRoot();
+
+		public static IEnumerable<MemberDeclarationSyntax> AsMemberDeclarationSyntaxes(this CompilationUnitSyntax compilationUnitSyntax)
+			=> compilationUnitSyntax
+			.ChildNodes()
+			.OfType<MemberDeclarationSyntax>();
+
+		// TODO: make this more versatile by replacing Pascalize calls with
+		// a Func<string, string> parameter to do the casing for us
+		public static void ApplyCSharpCasing(this DecoratorFile @in)
+		{
+			@in.Namespace = @in.Namespace.Split('.')
+				.Select(x => x.Pascalize())
+				.Aggregate((a, b) => $"{a}.{b}");
+
+			foreach (var x in @in.Classes)
+			{
+				x.Name = x.Name.Pascalize();
+			}
+
+			foreach (var i in @in.Classes.SelectMany(x => x.Fields))
+			{
+				i.Name = i.Name.Pascalize();
+			}
+		}
 	}
 }
